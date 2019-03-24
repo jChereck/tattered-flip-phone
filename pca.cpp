3bef61e6c081cce600244585983dae9b43bad37f
jchereck <josephchereck@gmail.com>
2019-03-24 12:57:59 -0700

    got everything going up to producing a covariance matrix (I think correctly)

--- pca.cpp
+++ pca.cpp
@@ -1,5 +1,6 @@
 #include "rand.h"
 #include "mat.h"
+#include <math.h>
 
 
 int main(int argc, char *argv[]){
@@ -9,8 +10,34 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	printf("K = %d!\n", atoi(argv[1]));
+	int K = atoi(argv[1]);
 
+	//Read in matrix of image
+	Matrix mIn("Raw Image Data");
+	bool mInColor;
+	mIn.readImagePixmap("", "mIn", mInColor);
+
+	//If K is negative transpose input and set flag to undo at the end
+	bool transposed = false;
+	if( K > 0 ){
+		mIn.transposeSelf();
+		K = abs(K);
+		transposed = true;
+	}
+
+	//TODO:Use Z-score/stdev to normalize?
+	//Mean Normalization of input matrix
+	double mInMean = mIn.mean();
+	mIn.scalarPostSub(mInMean);
+
+	//compute covariance Matrix
+	Matrix mCov = mIn.cov();
+
+
+
+
+
+	//Untranspose output before finishing if transposed flag is set
 
 	return 0;
 }
