56e74418edb7ac5da1e2ba01491925944594e231
jchereck <josephchereck@gmail.com>
2019-03-24 11:44:19 -0700

    got command line input of K working

--- pca.cpp
+++ pca.cpp
@@ -1,1 +1,16 @@
-//PCA cpp file
+#include "rand.h"
+#include "mat.h"
+
+
+int main(int argc, char *argv[]){
+
+	if(argc != 2){
+		printf("Please enter K on the command line and nothing else\n");
+		return 1;
+	}
+
+	printf("K = %d!\n", atoi(argv[1]));
+
+
+	return 0;
+}
